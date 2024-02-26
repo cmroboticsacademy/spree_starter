e613c9d6b4f2349c9c3e8f8ca5ed1dc9142ab5fe
Vu Nguyen <vnguyen@nrec.ri.cmu.edu>
2024-02-26 14:27:57 -0500

    Removing it

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -1,8 +1,2 @@
 Rails.application.config.active_storage.resolve_model_to_route = :rails_storage_proxy
 Rails.application.config.active_storage.variant_processor = :vips
-
-module ActiveStorage::Blob::Analyzable
-  def analyze_later
-    analyze
-  end
-end
